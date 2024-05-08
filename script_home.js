a793f68af8e28bca6d886ab0799a473610f6203c
Jonak <126195899+Xingho1@users.noreply.github.com>
2024-05-08 16:22:56 +0000

    remdelling 1

--- script_home.js
+++
@@ -1,4 +0,0 @@
-// This script could be expanded to add more interactivity to the site
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document loaded and ready');
-});
